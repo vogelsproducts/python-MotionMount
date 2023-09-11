321a308c66ddd51188c68bb6b56898bde95b5ab4
Remco Poelstra <remco@beryllium.net>
2023-09-11 13:41:04 +0200

    Restructured to make it a Python package.

---
+++ src/motionmount/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Vogel's Products
+#
+# This file is part of python-MotionMount
+#
+# SPDX-License-Identifier:    MIT
+#
+
+from motionmount.motionmount import *
--- src/motionmount/motionmount.py
+++ src/motionmount/motionmount.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Vogel's Products
+#
+# This file is part of python-MotionMount
+#
+# SPDX-License-Identifier:    MIT
+#
+
 import collections
 
 import asyncio
