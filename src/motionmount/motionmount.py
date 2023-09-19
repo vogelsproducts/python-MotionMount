5a0e7933e73ad25aaf9f5b1b9c7acb1c66dbd31e
Remco Poelstra <r.poelstra@vogels.com>
2023-09-19 12:06:46 +0200

    Added documentation mostly by ChatGPT.

--- src/motionmount/motionmount.py
+++ src/motionmount/motionmount.py
@@ -17,6 +17,10 @@ from enum import Enum, IntEnum
 
 
 class MotionMountResponse(IntEnum):
+    """
+    Enum representing possible response codes from MotionMount.
+    These are derived from HTTP response code and have a corresponding meaning.
+    """
     Unknown = 0,
     Accepted = 202,
     BadRequest = 400,
@@ -28,6 +32,9 @@ class MotionMountResponse(IntEnum):
 
 
 class MotionMountValueType(Enum):
+    """
+    Enum representing possible value types for MotionMount requests.
+    """
     Integer = 0,
     String = 1,
     ByteArray = 2,
@@ -38,28 +45,45 @@ class MotionMountValueType(Enum):
 
 class MotionMountError(Exception):
     """
+    Base exception class for MotionMount errors.
     """
+    pass
 
 
 class NotConnectedError(MotionMountError):
     """
+    Exception raised when not connected to MotionMount.
     """
-
     def __str__(self):
         return "Not connected to MotionMount"
 
 
 class MotionMountResponseError(MotionMountError):
     """
-    """
+    Exception raised for MotionMount response errors.
 
+    Attributes:
+        response_value (MotionMountResponse): The response code received.
+    """
     def __init__(self, value: MotionMountResponse):
         self.response_value = value
 
 
 class Request:
-    """Internal class that represents a request that's sent to the MotionMount."""
+    """
+    Represents a request to be sent to the MotionMount.
 
+    Args:
+        key (str): The key for the request.
+        value_type (MotionMountValueType): The value type for the request.
+        value (Optional[str]): The optional value for the request.
+
+    Attributes:
+        key (str): The key for the request.
+        value_type (MotionMountValueType): The value type for the request.
+        value (Optional[str]): The optional value for the request.
+        future (asyncio.Future): The asyncio Future associated with the request.
+    """
     def __init__(self, key: str, value_type: MotionMountValueType, value: Optional[str] = None):
         self.key = key
         self.value = value
@@ -69,8 +93,12 @@ class Request:
         self.future = event_loop.create_future()
 
     def encoded(self) -> bytes:
-        """Encodes this request such that it can be sent over the network."""
+        """
+        Encodes this request for sending over the network.
 
+        Returns:
+            bytes: The encoded request.
+        """
         if self.value is None:
             return f"{self.key}\n".encode()
         else:
@@ -78,12 +106,22 @@ class Request:
 
 
 def _convert_value(value, value_type: MotionMountValueType):
+    """
+    Convert a value to the specified MotionMount value type.
+
+    Args:
+        value: The value to convert.
+        value_type (MotionMountValueType): The target value type.
+
+    Returns:
+        Any: The converted value.
+    """
     if value_type == MotionMountValueType.Integer:
         return int(value)
     elif value_type == MotionMountValueType.String:
         return value.strip("\"")
     elif value_type == MotionMountValueType.ByteArray:
-        return ValueError("Byte array not supported")
+        raise ValueError("Byte array not supported")
     elif value_type == MotionMountValueType.Bool:
         return bool(value)
     elif value_type == MotionMountValueType.Void:
@@ -93,15 +131,19 @@ def _convert_value(value, value_type: MotionMountValueType):
 
 
 class MotionMount:
-    """Class to represent a MotionMount
-
-        You can create one with the IP address and port number which should be used to connect.
-
-        After creation, you should call `connect` first, before accessing other properties.
-
-        When you're done, call `disconnect` to clean up resources.
     """
+    Class to represent a MotionMount.
 
+    You can create one with the IP address and port number which should be used to connect.
+
+    After creation, you should call `connect` first, before accessing other properties.
+
+    When you're done, call `disconnect` to clean up resources.
+
+    Args:
+        address (str): The IP address of the MotionMount.
+        port (int): The port number to use for the connection.
+    """
     def __init__(self, address: str, port: int):
         self.address = address
         self.port = port
@@ -115,6 +157,9 @@ class MotionMount:
         self.turn = None
 
     async def connect(self):
+        """
+        Connect to the MotionMount.
+        """
         reader, writer = await asyncio.open_connection(self.address, self.port)
 
         self._writer = writer
@@ -123,6 +168,9 @@ class MotionMount:
         await self.update_position()
 
     async def disconnect(self):
+        """
+        Disconnect from the MotionMount.
+        """
         writer = self._writer
 
         # Close the stream
@@ -145,38 +193,96 @@ class MotionMount:
             await writer.wait_closed()
 
     async def get_name(self) -> str:
+        """
+        Get the name of the MotionMount.
+
+        Returns:
+            str: The name of the MotionMount.
+        """
         return await self._request(Request("configuration/name", MotionMountValueType.String))
 
     async def update_position(self):
+        """
+        Fetch the current position of the MotionMount.
+        """
         # We mark the value types as Void, as we've no further interest in the actual value
         # We just want to trigger the notification logic
         await self._request(Request("mount/extension/current", MotionMountValueType.Void))
         await self._request(Request("mount/turn/current", MotionMountValueType.Void))
 
     async def go_to_preset(self, position: int):
+        """
+        Go to a preset position.
+        Preset 0 is the (fixed) Wall position.
+
+        Args:
+            position (int): The preset position to go to (0 - 9).
+
+        Raises:
+            ValueError: If the position is out of range.
+        """
         if position < 0 or position > 9:
             raise ValueError("position must be in the range [0...9]")
 
         await self._request(Request(f"mount/preset/index = {position}", MotionMountValueType.Void))
 
     async def go_to_position(self, extension: int, turn: int):
-        if extension < 0 or extension > 100: raise ValueError("extension must be in the range [0...100]")
-        if turn < -100 or turn > 100: raise  ValueError("turn must be in the range [-100...100]")
+        """
+        Go to a specific position.
+
+        Args:
+            extension (int): The extension value (0 - 100)
+            turn (int): The turn value. (-100 - 100)
+
+        Raises:
+            ValueError: If the extension or turn values are out of range.
+        """
+        if extension < 0 or extension > 100:
+            raise ValueError("extension must be in the range [0...100]")
+        if turn < -100 or turn > 100:
+            raise ValueError("turn must be in the range [-100...100]")
 
         value_bytes = struct.pack('>Hh', extension, turn)
         await self._request(Request(f"mount/preset/position = [{value_bytes.hex()}]", MotionMountValueType.Void))
 
     async def set_extension(self, extension: int):
-        if extension < 0 or extension > 100: raise ValueError("extension must be in the range [0...100]")
+        """
+        Set the extension value.
+
+        Args:
+            extension (int): The extension value (0 - 100).
+
+        Raises:
+            ValueError: If the extension value is out of range.
+        """
+        if extension < 0 or extension > 100:
+            raise ValueError("extension must be in the range [0...100]")
         await self._request(Request(f"mount/extension/target = {extension}", MotionMountValueType.Void))
 
     async def set_turn(self, turn: int):
-        if turn < -100 or turn > 100: raise  ValueError("turn must be in the range [-100...100]")
+        """
+        Set the turn value.
+
+        Args:
+            turn (int): The turn value (-100 - 100).
+
+        Raises:
+            ValueError: If the turn value is out of range.
+        """
+        if turn < -100 or turn > 100:
+            raise ValueError("turn must be in the range [-100...100]")
         await self._request(Request(f"mount/turn/target = {turn}", MotionMountValueType.Void))
 
     async def _request(self, request: Request):
-        """ Enqueues `request`, waits for possible earlier requests and then waits for `request` to finish"""
+        """
+        Enqueues a request, waits for possible earlier requests, and then waits for the request to finish.
 
+        Args:
+            request (Request): The request to send.
+
+        Returns:
+            Any: The response value.
+        """
         # Ignore requests when we're not connected
         if self._writer is None:
             raise NotConnectedError
@@ -203,15 +309,26 @@ class MotionMount:
         return value
 
     def _update_properties(self, key: str, value: str):
+        """
+        Update internal properties based on key-value pairs received from MotionMount.
+
+        Args:
+            key (str): The key from MotionMount.
+            value (str): The corresponding value.
+        """
         if key == "mount/extension/current":
             self.extension = _convert_value(value, MotionMountValueType.Integer)
         elif key == "mount/turn/current":
             self.turn = _convert_value(value, MotionMountValueType.Integer)
-        # TODO: How to let the world now that a property changed????
+        # TODO: How to let the world know that a property changed????
 
     async def _reader(self, reader: asyncio.StreamReader):
-        """ Infinite loop to receive data from the MotionMount and dispatch it to the waiting requests"""
+        """
+        Infinite loop to receive data from the MotionMount and dispatch it to waiting requests.
 
+        Args:
+            reader (asyncio.StreamReader): The stream reader for receiving data.
+        """
         while not reader.at_eof():
             data = await reader.readline()
             response = data.decode().strip()
